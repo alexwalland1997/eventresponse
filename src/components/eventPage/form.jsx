e2f6a41c13250eeb677bf8aaf2993f7dad6cbc69
alexwalland <alexanderwalland@gmail.com>
2024-03-09 20:26:51 +0000

    completed site
    added form functionality so get email when form is submitted map of location and email invite button as well as sortinf out mobile functionality

--- src/components/eventPage/form.jsx
+++ src/components/eventPage/form.jsx
@@ -1,33 +1,85 @@
 import { Label, Textarea, TextInput, Button, Select } from "flowbite-react";
-import "./index.css"
-import party from "party-js";
+import React, { useRef } from 'react';
+import { AddToCalendarButton } from "add-to-calendar-button-react";
+import emailjs from "@emailjs/browser"
 
-export default function form() {
-function submitForm(e) {
- e.preventDefault();
- party.sparkles(e.target);
-}
+export default function Form() {
+  const form = useRef();
+
+  const sendEmail = (e) =>{
+    e.preventDefault();
+    emailjs
+        .sendForm(
+          'service_20d6lpp',
+          'template_uywzjbq',
+           form.current,
+          'lB9zEbiDA5rpQkSct'
+        )
+        .then (
+            () => {
+            },
+            () => {
+                alert('Error message not sent');
+            }
+        )
+
+  }
 
   return (
-    <div className="flex max-w-md flex-col m-8 cForm">
-      <form>
-      <div className="mb-4 block">
-        <Label htmlFor="name" value="Name/Family Name"/>
-        <TextInput id="name" placeholder="name"/>
+    <>
+      <div className="cForm">
+        <form ref={form} onSubmit={sendEmail}>
+          <div className="mb-4 block">
+            <Label htmlFor="name" value="Name/Family Name" />
+            <TextInput id="name" name="name" placeholder="name" />
+          </div>
+          <div className="mb-4 block">
+            <Label htmlFor="attendance" value="Are you able to attend" />
+            <Select id="attendance" name="attendance" required>
+              <option>Yes</option>
+              <option>No</option>
+            </Select>
+          </div>
+          <div className="mb-4 block">
+            <Label htmlFor="allergies" value="Please list any allergies" />
+            <Textarea
+              id="allergies"
+              name="allergies"
+              rows="10"
+              placeholder="Leave any allergies"
+            />
+          </div>
+          <Button type="submit" id="submit">Submit</Button>
+        </form>
       </div>
-      <div className="mb-4 block">
-        <Label htmlFor="attendance" value="Are you able to attend"/>
-        <Select id="attendance" required>
-            <option>Yes</option>
-            <option>No</option>
-        </Select>
+      <div id="info">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2466.1022672443964!2d0.7942248765387204!3d51.82256227188666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d91d16a33f1d9b%3A0xd075cbfdc783b725!2sLayer%20Marney%20Tower%20Weddings!5e0!3m2!1sen!2suk!4v1710008834105!5m2!1sen!2suk"
+          id="venue"
+          width="300"
+          height="300"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+        <div id="inviteBtn">
+        <AddToCalendarButton
+          name="Sam and Alex's Wedding"
+          description="Samantha Hatton and Alexander's Wedding"
+          options={['Apple','Google','iCal','Outlook.com','Yahoo']}
+          location="Layer Marney Tower C05 9US"
+          startDate="2025-05-04"
+          endDate="2025-05-05"
+          startTime="13:00"
+          endTime="00:00"
+          timeZone="Europe/London"
+          buttonStyle="3d"
+          listStyle="dropup-static"
+          hideBackground
+          size="5"
+        ></AddToCalendarButton>
         </div>
-        <div className="mb-4 block">
-        <Label htmlFor="allergies" value="Please list any allergies"/>
-        <Textarea id="allergies" rows="10" placeholder="Leave any allergies"/>
-        </div>
-        <Button type="submit" id="submit" onClick={(e)=>submitForm(e)}>Submit</Button>
-      </form>
-    </div>
+      </div>
+    </>
   );
 }
--- src/components/eventPage/index.jsx
+++ src/components/eventPage/index.jsx
@@ -1,12 +1,10 @@
-import "./index.css"
 import Form from "./form"
 import event from "./../../data/data.json"
-import party from "party-js";
 
 export default function eventPage() {
     return (
         <>
-        <div id="invite" onClick={(e)=>party.confetti(e.target)}>
+        <div id="invite">
             <h1>You're invited to {event.name}</h1>
             <h2>The event is on {event.date} and will be held at {event.venue}</h2>
         </div>
